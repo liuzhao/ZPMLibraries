71ba3bf55af8d2825200aa73ce2adb780c22b6d1
zhao.liu <zhao.liu@zhaopin.com.cn>
2019-05-23 16:10:29 +0800

    提交XYDebug缺失文件

---
+++ ZPMLibraries/ViewControllers/调试库/Others/XYDebug/XYDebugCloneView.h
@@ -0,0 +1,16 @@
+//
+//  XYDebugCloneView.h
+//  Pods
+//
+//  Created by XcodeYang on 25/05/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYDebugCloneView : UIView
+
++ (XYDebugCloneView *)cloneWith:(UIView *)view;
+
+@end
+
